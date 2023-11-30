698851309c8267bc8f13b93f1e997436b66318e4
Giorios <90624324+Giorios@users.noreply.github.com>
2023-11-30 08:15:22 -0500

    javascript finished

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function valueGen() {
-    return Math.floor(100000 + Math.random() * 900000)
+    return Math.floor(100000 + Math.random() * 900000);
 }
 
 function colorGen() {
     document.getElementById("head1").innerHTML = "#" + valueGen();
-    document.getElementById("head2").style.color = "#" + valueGen();
     document.getElementById("box").style.backgroundColor = "#" + valueGen();
+    document.getElementById("head2").style.color = "#" + valueGen();
 }
 
-function colorSwap() {
-
-}
